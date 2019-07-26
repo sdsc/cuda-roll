aa96962ca91f7bfa6e4113cb6c3e038b9e7de449
Trevor Cooper <tcooper@sdsc.edu>
2019-07-25 17:32:51 -0700

    specify the RELEASE number

--- version.mk
+++ version.mk
@@ -2,6 +2,7 @@ DISTRO        = cuda-rhel7
 ARCH          = x86_64
 #VERSION       = rhel7
 VERSION      :=$(shell date +%Y.%m.%d)
+RELEASE       = 0
 ifndef MIRRORURL
   MIRRORURL   = https://developer.download.nvidia.com/compute/cuda/repos
 endif
